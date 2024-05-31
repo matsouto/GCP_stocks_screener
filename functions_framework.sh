0050d8f4c8a7951793881f32da5e3bc580af67ca
matsouto <matsouto55@gmail.com>
2024-05-31 01:43:06 -0300

    start creation of cloud enviroment

---
+++ functions_framework.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+functions-framework --port 8080 --target hello_get --signature-type http --source main.py --debug
+
+# curl -X POST \
+# -H "Content-type:application/json" \
+# -d  '{"num_1":20, "num_2": 30}' \
+# -w '\n' \
+# http://localhost:8080
